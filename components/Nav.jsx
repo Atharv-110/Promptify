f3a3f0562adcb2963bbfed452ea1b5cd1d611e38
Atharv Vani <atharvavani110@gmail.com>
2023-08-13 23:54:08 +0530

    design updated in navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -24,8 +24,8 @@ const Nav = () => {
         <Image
           src='/assets/images/logo.svg'
           alt='logo'
-          width={40}
-          height={40}
+          width={50}
+          height={50}
           className='object-contain'
         />
         <p className='logo_text'>Promptify</p>
